1dfa355d67433f1781079d8a8b68ca4a0da3a38c
Jelmer Snippe <jelmersnippe@hotmail.com>
2024-07-09 18:25:38 +0200

    Add grace time when charging in the air

--- player.gd
+++ player.gd
@@ -30,25 +30,31 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	return
-	
-	if Input.is_action_just_pressed("charge") and not is_on_floor():
-		remaining_air_hold_grace_time = AIR_HOLD_GRACE_TIME
-
-	if Input.is_action_pressed("charge") and available_charges > 0:
-		current_charge += CHARGE_SPEED * delta
-		if current_charge > MAX_CHARGE:
-			current_charge = MAX_CHARGE
-		elif not is_on_floor():
-			velocity = Vector2(0, 0)
-			
-		queue_redraw()
+func _handle_airborne(delta: float):
+	if is_on_floor():
+		return
 		
-
-func _on_punch_area_area_entered(area):
-	if area.is_in_group("target"):
-		area.queue_free()
-		available_charges += 1
+	if remaining_air_time <= 0:
+		velocity.y += gravity * delta
+	else:
+		remaining_air_time -= delta
+		if remaining_air_time <= 0:
+			velocity /= 4
+			
+	if Input.is_action_just_pressed("charge"):
+		remaining_air_hold_grace_time = AIR_HOLD_GRACE_TIME
+			
+	if Input.is_action_pressed("charge"):
+		var did_charge = _charge(delta)
+		if did_charge:
+			velocity = Vector2.ZERO
+		elif remaining_air_hold_grace_time > 0:
+			velocity = Vector2.ZERO
+			remaining_air_hold_grace_time -= delta
+		
+	if Input.is_action_just_released("charge"):
+		_release_charge()
+		remaining_air_hold_grace_time = 0
 		
 func _handle_grounded(delta: float):
 	if not is_on_floor():
@@ -64,18 +70,25 @@ func _handle_grounded(delta: float):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if Input.is_action_pressed("charge") and available_charges > 0:
+	if Input.is_action_pressed("charge"):
 		_charge(delta)
 		
 	if Input.is_action_just_released("charge"):
 		_release_charge()
 		
-func _charge(delta: float):
-	current_charge += CHARGE_SPEED * delta
-	if current_charge > MAX_CHARGE:
+func _charge(delta: float) -> bool:
+	if available_charges <= 0:
+		return false
+		
+	if current_charge >= MAX_CHARGE:
 		current_charge = MAX_CHARGE
+		queue_redraw()
+		return false
+		
+	current_charge = min(current_charge + CHARGE_SPEED * delta, MAX_CHARGE)
 		
 	queue_redraw()
+	return true
 	
 func _release_charge():
 	if current_charge <= 0 or available_charges <= 0:
@@ -88,14 +101,8 @@ func _release_charge():
 	var direction = (get_global_mouse_position() - global_position).normalized()
 	velocity = direction * MAX_CHARGE
 	queue_redraw()
-	
-func _handle_airborne(delta: float):
-	if is_on_floor():
-		return
 		
-	if remaining_air_time <= 0:
-		velocity.y += gravity * delta
-	else:
-		remaining_air_time -= delta
-		if remaining_air_time <= 0:
-			velocity /= 4
+func _on_punch_area_area_entered(area):
+	if area.is_in_group("target"):
+		area.queue_free()
+		available_charges += 1
