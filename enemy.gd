4603838c0c243f502eece188ccb1fa94f3e01097
Jelmer Snippe <jelmersnippe@hotmail.com>
2024-07-10 19:38:34 +0200

    Setup basic top down movement and collisions

--- enemy.gd
+++ enemy.gd
@@ -1,9 +1,8 @@
-extends Area2D
+extends CharacterBody2D
 class_name Enemy
 
 @export var speed = 50
 @export var knockback_recovery_speed = 5
-@export var direction = Vector2.RIGHT
 
 @export var base_health = 10:
 	set(value):
@@ -14,10 +13,6 @@ var current_health:
 		current_health = value
 		$Healthbar.value = current_health
 
-var default_wall_raycast: Vector2
-var default_floor_raycast: Vector2
-
-var velocity = Vector2.ZERO
 var knocked = false:
 	set(value):
 		knocked = value
@@ -32,55 +27,25 @@ var remaining_knocked_grace_time = 0
 
 func _ready():
 	current_health = base_health
-	default_wall_raycast = $WallRaycast.target_position
-	default_floor_raycast = $FloorRaycast.target_position
-	
-	$Sprite2D.flip_h = direction.x < 0
 
 func is_grounded() -> bool:
 	return $GroundedRaycast.is_colliding()
 
 func _process(delta):
+	if velocity.x > 0:
+		$Sprite2D.flip_h = false
+		
+	elif velocity.x < 0:
+		$Sprite2D.flip_h = true
+	
 	if knocked:
 		knocked_grace_time -= delta
 		velocity.x = move_toward(velocity.x, 0, knockback_recovery_speed)
-		if velocity.x == 0:
+		velocity.y = move_toward(velocity.y, 0, knockback_recovery_speed)
+		if velocity == Vector2.ZERO:
 			knocked = false
-	
-	# apply gravity
-	if not is_grounded():
-		velocity.y += 9.8
 		
-	if not knocked:
-		velocity.x = direction.x * speed
-		
-	_set_raycasts_to_direction()
-	
-	# turn on wall collision or no floor in front while in control on the ground
-	if $WallRaycast.is_colliding() or (not knocked and is_grounded() and not $FloorRaycast.is_colliding()):
-		_turn(-velocity)
-	
-	position += velocity * delta
-	
-	# correct any vertical clipping if moving down
-	if velocity.y >= 0 and is_grounded():
-		velocity.y = 0
-		global_position.y = $GroundedRaycast.get_collision_point().y - $GroundedRaycast.target_position.y
-		
-func _turn(turn_direction: Vector2):
-	var turn_sign = sign(turn_direction.x)
-	$Sprite2D.flip_h = turn_sign == -1
-	velocity.x = abs(velocity.x) * turn_sign
-	direction = Vector2(turn_sign, 0)
-	_set_raycasts_to_direction()
-	
-func _set_raycasts_to_direction():
-	if velocity.x > 0:
-		$WallRaycast.target_position.x = default_wall_raycast.x
-		$FloorRaycast.target_position.x = default_floor_raycast.x
-	else:
-		$WallRaycast.target_position.x = -default_wall_raycast.x
-		$FloorRaycast.target_position.x = -default_floor_raycast.x
+	move_and_slide()
 	
 func knockback(normalized_impact_direction: Vector2, force: float):
 	if knocked: 
@@ -88,7 +53,6 @@ func knockback(normalized_impact_direction: Vector2, force: float):
 		
 	knocked = true
 	velocity = normalized_impact_direction * force
-	_set_raycasts_to_direction()
 
 func take_damage(damage: int):
 	current_health = clamp(current_health - damage, 0, base_health)
@@ -96,15 +60,15 @@ func take_damage(damage: int):
 	if current_health <= 0:
 		queue_free()
 
-func _on_area_entered(area):
+func _on_hitbox_area_entered(area):
 	if not knocked:
 		return
 		
-	if area is Enemy:
-		var enemy = area as Enemy
+	var parent = area.get_parent()
+	if parent is Enemy:
+		var enemy = parent as Enemy
 		if enemy.remaining_knocked_grace_time > 0:
 			return
 		enemy.knockback(velocity.normalized(), (velocity.x + velocity.y) / 2)
 		enemy.take_damage((velocity.x + velocity.y) / 120)
-		enemy._turn(velocity.normalized())
 		velocity /= 3
--- player.gd
+++ player.gd
@@ -6,26 +6,19 @@ signal just_landed()
 @export var CHARGE_SLOWDOWN = 0.3
 @export var TIME_TO_MAX_CHARGE = 1
 @export var MAX_CHARGE = 600
-@export var AIR_TIME = 0.2
-@export var AIR_HOLD_GRACE_TIME = 0.2
+@export var PUNCH_TIME = 0.2
 
 var CHARGE_SPEED: int = MAX_CHARGE / TIME_TO_MAX_CHARGE
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @export var current_charge: float = 0
-@export var remaining_air_time: float = 0:
+@export var remaining_punch_time: float = 0:
 	set(value):
-		remaining_air_time = value
-		if remaining_air_time <= 0:
+		remaining_punch_time = value
+		if remaining_punch_time <= 0:
 			$Sprite2D/Hand.position = Vector2(-3, 3)
 			$Sprite2D/Hand2.position = Vector2(3, 3)
-@export var remaining_air_hold_grace_time: float = 0
 
 @export var available_charges = 0
-
-var is_grounded = false
 
 func _set_release_hands():
 	if velocity.x > 0:
@@ -45,72 +38,40 @@ func _set_charging_hands():
 		$Sprite2D/Hand2.position = Vector2(8, 3)
 		
 func _draw():
-	var distance = current_charge * AIR_TIME
+	var distance = current_charge * PUNCH_TIME
 	var direction = (get_global_mouse_position() - global_position).normalized()
 	draw_line(Vector2(0, 0), distance * direction, Color.REBECCA_PURPLE, 2)
 	
 	$Sprite2D.flip_h = direction.x < 0
 
 func _physics_process(delta):
-	if is_on_floor():
-		if not is_grounded:
-			just_landed.emit()
-		_handle_grounded(delta)
-	else:
-		_handle_airborne(delta)
-		
-	is_grounded = is_on_floor()
+	_handle_grounded(delta)
 	
-	if remaining_air_time > 0:
-		remaining_air_time -= delta
-		if remaining_air_time <= 0:
+	if remaining_punch_time > 0:
+		remaining_punch_time -= delta
+		if remaining_punch_time <= 0:
 			velocity /= 4
 			
 	move_and_slide()
-	
-func _handle_airborne(delta: float):
-	if is_on_floor():
-		return
-		
-	if remaining_air_time <= 0:
-		velocity.y += gravity * delta
-			
-	if Input.is_action_just_pressed("charge") and available_charges > 0:
-		remaining_air_hold_grace_time = AIR_HOLD_GRACE_TIME
-			
-	if Input.is_action_pressed("charge"):
-		var did_charge = _charge(delta)
-		if did_charge:
-			velocity = Vector2.ZERO
-		elif remaining_air_hold_grace_time > 0:
-			velocity = Vector2.ZERO
-			remaining_air_hold_grace_time -= delta
-		
-	if Input.is_action_just_released("charge"):
-		_release_charge()
-		remaining_air_hold_grace_time = 0
 		
 func _handle_grounded(delta: float):
-	if not is_on_floor() or remaining_air_time > 0:
+	if remaining_punch_time > 0:
 		return
 		
-	remaining_air_time = 0
-	remaining_air_hold_grace_time = 0
+	remaining_punch_time = 0
 	available_charges = 1
 		
-	var input = Input.get_axis("move_left", "move_right")
-	if input:
-		velocity.x = input * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if input == 1:
+	var input = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
+	velocity = input * SPEED
+		
+	if input.x > 0:
 		$Sprite2D.flip_h = false
-	elif input == -1:
+	elif input.x < 0:
 		$Sprite2D.flip_h = true
 		
 	if Input.is_action_pressed("charge"):
-		velocity.x *= CHARGE_SLOWDOWN
+		velocity *= CHARGE_SLOWDOWN
 		_charge(delta)
 		
 	if Input.is_action_just_released("charge"):
@@ -136,7 +97,7 @@ func _release_charge():
 	if current_charge <= 0 or available_charges <= 0:
 		return
 		
-	remaining_air_time = (current_charge / MAX_CHARGE) * AIR_TIME
+	remaining_punch_time = (current_charge / MAX_CHARGE) * PUNCH_TIME
 	current_charge = 0
 	available_charges -= 1
 	
@@ -146,17 +107,15 @@ func _release_charge():
 	queue_redraw()
 		
 func _on_punch_area_area_entered(area: Area2D):
-	if area.is_in_group("target"):
-		available_charges += 1
-		area.queue_free()
-	elif remaining_air_time > 0:
+	if remaining_punch_time > 0:
+		var parent = area.get_parent()
 		if area.is_in_group("breakable"):
-			area.get_parent().queue_free()
-		elif area is Enemy:
-			var enemy = area as Enemy
-			var impact_force = (remaining_air_time / AIR_TIME) * MAX_CHARGE
+			parent.queue_free()
+		elif parent is Enemy:
+			var enemy = parent as Enemy
+			var impact_force = (remaining_punch_time / PUNCH_TIME) * MAX_CHARGE
 			enemy.knockback(position.direction_to(enemy.position), impact_force)
 			enemy.take_damage(impact_force / 120)
 			velocity /= 4
 		else:
-			area.get_parent().apply_central_impulse(velocity)
+			parent.apply_central_impulse(velocity)
