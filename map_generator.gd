948c3c817867d96a4332a784712d94f88adb380e
Jelmer Snippe <jelmersnippe@hotmail.com>
2024-07-14 14:23:44 +0200

    Add walker algorithm for map generation and test scene

---
+++ map_generator.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+@export var steps: int = 20
+@export var initial_walkers: int = 4
+@export var chance_to_turn: float = 0.8
+@export var chance_to_spawn_walker: float = 0.2
+
+var walker_positions: Array[Vector2i] = []
+var walker_directions: Array[Vector2i] = []
+var walker_remaining_steps: Array[int] = []
+var walker_chance_to_spawn: Array[float] = []
+
+var directions = [Vector2i.DOWN, Vector2i.UP, Vector2i.LEFT, Vector2i.RIGHT]
+
+func _ready():
+	var tiles = generate_map(get_viewport_rect().get_center() / $TileMap.rendering_quadrant_size)
+	create_tilemap(tiles)
+	
+func create_tilemap(tiles: Array[Vector2i]):
+	for tile in $TileMap.get_used_cells(0):
+		$TileMap.set_cell(0, tile, -1)
+		
+	for tile in tiles:
+		$TileMap.set_cell(0, tile, 0, Vector2i(0, 0))
+	
+func generate_map(center: Vector2i) -> Array[Vector2i]:
+	var map_tiles = [center]
+	
+	for i in initial_walkers:
+		_spawn_walker(center, chance_to_spawn_walker)
+		
+	while len(walker_positions) > 0:
+		var indexes_to_clear = []
+		for i in len(walker_positions):
+			walker_positions[i] += walker_directions[i]
+			map_tiles.append(walker_positions[i])
+			
+			walker_remaining_steps[i] -= 1
+			
+			if walker_remaining_steps[i] <= 0:
+				indexes_to_clear.append(i)
+			else:
+				var turn_roll = randf_range(0, 1)
+				if turn_roll < chance_to_turn:
+					walker_directions[i] = _get_random_direction()
+					
+				var spawn_roll = randf_range(0, 1)
+				if spawn_roll < walker_chance_to_spawn[i]:
+					walker_chance_to_spawn[i] /= 2
+					_spawn_walker(walker_positions[i], walker_chance_to_spawn[i])
+		
+		indexes_to_clear.reverse()
+		for i in indexes_to_clear:
+			walker_positions.remove_at(i)
+			walker_directions.remove_at(i)
+			walker_remaining_steps.remove_at(i)
+			walker_chance_to_spawn.remove_at(i)
+			
+	var unique: Array[Vector2i] = []
+
+	for item in map_tiles:
+		if not unique.has(item):
+			unique.append(item)
+
+	return unique
+	
+func _spawn_walker(initial_position: Vector2i, spawn_chance: float):
+	walker_positions.append(initial_position)
+	walker_directions.append(_get_random_direction())
+	walker_remaining_steps.append(steps)
+	walker_chance_to_spawn.append(spawn_chance)
+
+func _get_random_direction() -> Vector2i:
+	var direction_index = randi_range(0, len(directions) - 1)
+	return directions[direction_index]
+
+
+func _on_generate_pressed():
+	var tiles = generate_map(get_viewport_rect().get_center() / $TileMap.rendering_quadrant_size)
+	create_tilemap(tiles)
